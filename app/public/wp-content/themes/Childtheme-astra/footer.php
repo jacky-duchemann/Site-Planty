a808b12970dc85c1962cb6fcb1661ca874560b8a
jacky-duchemann <jackyduchemann@hotmail.fr>
2024-04-02 18:08:36 +0400

    Integration de la page d'acceuil avec elementor

--- app/public/wp-content/themes/Childtheme-astra/footer.php
+++ app/public/wp-content/themes/Childtheme-astra/footer.php
@@ -1,7 +1,36 @@
-<link rel="stylesheet" href="header.css">
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="footer-content">
-        <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Mentions légales</p>
-    </div>
-</footer>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+	</body>
+</html>
--- app/public/wp-content/themes/Childtheme-astra/header.php
+++ app/public/wp-content/themes/Childtheme-astra/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
-	<link rel="stylesheet" href="header.css">
+	
 </head>
 <body <?php body_class(); ?>>
     <header>
