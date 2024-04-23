58abd29f1e265b015ccffafc5759db8bdfd1463f
jacky-duchemann <jackyduchemann@hotmail.fr>
2024-04-23 16:59:47 +0400

    Ajustement des derniers détails et épuration du code

--- app/public/wp-content/themes/Childtheme-astra/functions.php
+++ app/public/wp-content/themes/Childtheme-astra/functions.php
@@ -8,12 +8,12 @@ function theme_enqueue_styles() {
 
 <?php
 add_filter( 'wp_nav_menu_items', 'admin_menu_item', 10, 2 );
-function admin_menu_item ( $items, $args ) {
-    if ($args->theme_location == 'primary') {
+function admin_menu_item( $items, $args ) {
+    if ( $args->theme_location == 'primary' && is_user_logged_in() ) {
         // Trouver l'index du premier lien dans le menu
-        $first_link_position = strpos($items, '</a>') + 4;
+        $first_link_position = strpos( $items, '</a>' ) + 4;
         // Insérer le lien admin juste après le premier lien
-        $items = substr_replace($items, '<li id="menu-item-18" ><a href="' . admin_url() . '">Admin</a></li>', $first_link_position, 0);
+        $items = substr_replace( $items, '<li id="menu-item-18" ><a href="' . admin_url() . '">Admin</a></li>', $first_link_position, 0 );
     }
     return $items;
 }
